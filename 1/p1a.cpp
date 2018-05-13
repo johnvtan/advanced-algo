66e61f62b336c0e6ed22f53692b5010a001f3b95
johnvtan <john.v.tan@gmail.com>
2018-05-13 15:32:17 -0400

    first commit. working on p1

---
+++ 1/p1a.cpp
@@ -0,0 +1,125 @@
+// Project 1a: Solving knapsack using exhaustive search
+//
+
+#include <iostream>
+#include <limits.h>
+#include <list>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+#include "d_except.h"
+#include "d_matrix.h"
+#include "knapsack.h"
+#include "subset.h"
+
+/*
+void exhaustiveKnapsackRecursive(vector <int> items, int max) {
+   //int startItem = items.back();
+   for (int k = 0; k < items.size(); k++) {
+         cout << items[k] << " ";
+   }
+   cout << endl;
+   for (int i = items.back() + 1; i < max; i++) {
+      items.push_back(i);
+      doCombinationRecursive(items, max);
+      items.pop_back();
+   }
+}
+
+void exhaustiveKnapsack(knapsack &k, int t) {
+   vector <int> items(1, 0);
+   int max = k.getNumObjects();
+   for (int i = 0; i < max; i++) {
+      items[0] =  i;
+      doCombinationRecursive(items, max);
+   }
+}
+*/
+
+void printVector(vector <int> v) {
+   for (unsigned int i = 0; i < v.size(); i++) {
+      cout << v[i] << " ";
+   }
+   cout << endl;
+}
+
+// exhaustively searches knapsack for up to t seconds 
+// then prints out best solution
+void exhaustiveKnapsack(knapsack &k, int t) {
+   Subset subset(k.getNumObjects());
+   vector <int> s;
+   int best_value = 0;
+   vector <int> best_combination;
+   clock_t start_time;
+   start_time = clock();
+   double total_time = 0;
+
+   while (subset.hasNext()) {
+      // take everything out of knapsack
+      k.unSelectAll();
+      s = subset.nextSubset();
+      k.selectList(s);
+
+      if (k.getCost() <= k.getCostLimit() && k.getValue() > best_value) {
+         best_combination = s;
+         best_value = k.getValue();
+      }
+      total_time = (clock() - start_time) / (double) CLOCKS_PER_SEC;
+      if (total_time >= t) {
+         break;
+      }
+
+   }  
+   k.unSelectAll();
+   k.selectList(best_combination);
+   k.printSolution();
+   cout << "Total Time: " << total_time << "seconds\n";
+}
+
+int main(int argc, char **argv)
+{
+   ifstream fin;
+   stack <int> moves;
+   string fileName;
+
+   if (2 != argc) 
+   {
+      cout << "Error: need to include path to input as argument.\n"
+           << "\tp1a <input-file>\n";
+      exit(1);
+   }
+
+   fin.open(argv[1]);
+   if (!fin)
+   {
+      cerr << "Cannot open " << fileName << endl;
+      exit(1);
+   }
+
+   try
+   {
+      cout << "Reading knapsack instance" << endl;
+      knapsack k(fin);
+
+      //exhaustiveKnapsack(k, 600);
+
+      //cout << endl << "Best solution" << endl;
+      //k.printSolution();
+      exhaustiveKnapsack(k, 600);
+   }    
+
+   catch (indexRangeError &ex) 
+   { 
+      cout << ex.what() << endl; exit(1);
+   }
+   catch (rangeError &ex)
+   {
+      cout << ex.what() << endl; exit(1);
+   }
+}
---
+++ 1/subset.h
@@ -0,0 +1,52 @@
+#ifndef SUBSET_CLASSES
+#define SUBSET_CLASSES
+
+#include "d_except.h"
+class NoSuchElementError: public baseException
+{
+public:
+	NoSuchElementError(const string& msg = ""):
+		baseException(msg)
+	{}
+};
+
+class Subset 
+{
+   public:
+   	Subset(int m);
+   	vector <int> nextSubset(void);
+   	bool hasNext();
+   private:
+    vector <int> items;
+    int max;
+};
+
+Subset::Subset(int m) {
+	max = m;
+}
+
+vector <int> Subset::nextSubset(void) {
+	if (items.size() == 0) {
+		items.push_back(0);
+		return items;
+	}
+	if (items.size() == 1 && items[0] == max - 1) {
+		throw NoSuchElementError("No remaining subsets");
+	}
+	if (items.back() == max - 1) {
+		items.pop_back();
+		items[items.size() - 1] = items.back() + 1;
+	} else {
+		items.push_back(items.back() + 1);
+	}
+	return items;
+}
+
+bool Subset::hasNext(void) {
+	if (items.size() == 1 && items[0] == max - 1) {
+		return false;
+	}
+	return true;
+}
+
+#endif
