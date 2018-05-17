58f66663e3279119f895948a16d590e7e36e8ad6
johnvtan <john.v.tan@gmail.com>
2018-05-17 11:28:52 -0400

    Took out all the bit stuff, directly increment graph colors now

--- 1/b/p1b.cpp
+++ 1/b/p1b.cpp
@@ -38,6 +38,8 @@ struct VertexProperties
     bool marked;
     int weight;
     int color;
+
+    VertexProperties() : color(0) {}
 };
 
 // Create a struct to hold properties for each edge
@@ -79,72 +81,35 @@ void setNodeWeights(Graph &g, int w)
     }
 }
 
-void incIntArray(unsigned int *array, int size) {
-    if (NULL == array || 0 == size) {
-        throw nullPtrError("Passed in null ptr or size 0 to incIntArray\n");
-    }
-    bool carry = false;
-    array[0]++;
-    for (int i = 0; i < size; i++) {
-        if (carry == true) {
-            array[i]++;
-        }
-        if (array[i] == 0) {
-            carry = true;
+void incrementColors(Graph &g, int maxColor) {
+    pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
+    for (Graph::vertex_iterator vItr= vItrRange.first; vItr != vItrRange.second; ++vItr) {
+        g[*vItr].color++;
+        if (g[*vItr].color > maxColor) {
+            g[*vItr].color = 0;
         } else {
             break;
         }
     }
 }
 
-#define UINT_BITS (8 * sizeof(unsigned int))
-
-bool checkIfDone(unsigned int *array, int size, int numBits) {
-    unsigned int mask = UINT_MAX;
-    for (int i = 0; i < size; i++) {
-        if (numBits < UINT_BITS) {
-            mask = (1 << numBits) - 1;
-        }
-        if (array[i] & mask) {
+bool checkIfDone(Graph &g, int maxColor) {
+    pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
+    for (Graph::vertex_iterator vItr= vItrRange.first; vItr != vItrRange.second; ++vItr) {
+        if (g[*vItr].color != maxColor) {
             return false;
         }
-        numBits -= UINT_BITS;
     }
     return true;
 }
 
-int colorGraph(Graph &g, unsigned int *colorArray, int size, int colorBits, int maxColor) {
-    int currColor = -1;
-    int mask = (1 << colorBits) - 1;
-    int leftover = 0;
-    int i = 0;          // index into colorArray
-    int nBits = 0;      // bit index into current int
-    pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
-    for (Graph::vertex_iterator vItr= vItrRange.first; vItr != vItrRange.second; ++vItr) {
-        currColor = (colorArray[i] >> nBits) & mask;
-        leftover = UINT_BITS - nBits;
-        if (leftover < colorBits) {
-            // crossing integer boundary
-            nBits = colorBits - leftover;
-            currColor |= (colorArray[++i] << leftover) & mask;
-        } else {
-            nBits += colorBits;
-        }
-        if (currColor > maxColor) {
-            return -1;
-        }
-        g[*vItr].color = currColor;
-        cout << "Node (" << *vItr << ") color = " << currColor << endl;
-    }
-    return 0;
-}
-
-bool isValidColoring(Graph &g) {
+/*
+bool numConflicts(Graph &g) {
     pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
 
-    // check each node's color in the graph and its neighbor's colors
+    // check each node's color in the graph and its neighbor's colorsclear
     for (Graph::vertex_iterator vItr= vItrRange.first; vItr != vItrRange.second; ++vItr) {
-        pair<Graph::adjaceny_iterator, Graph::adjaceny_iterator> adjItrRange = adjacent_vertices(g[*vItr], g);
+        pair<Graph::adjacency_iterator, Graph::adjacency_iterator> adjItrRange = adjacent_vertices(*vItr, g);
 
         // loop through each neighbor for the current node and check the colors
         for (Graph::vertex_iterator adjItr= adjItrRange.first; adjItr != adjItrRange.second; ++adjItr) {
@@ -157,38 +122,39 @@ bool isValidColoring(Graph &g) {
 
     return true;
 }
+*/
+
+void printColors(Graph &g) {
+    cout << "---------------New Coloring---------------" << endl;
+    pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
+    for (Graph::vertex_iterator vItr= vItrRange.first; vItr != vItrRange.second; ++vItr) {
+        cout << "Node (" << *vItr << "): " << g[*vItr].color << endl;
+    }
+}
 
 int exhaustiveColoring(Graph &g, int numColors, int t) {
-    int colorBits = ceil(log2(1.0*numColors));
-    cout << "Color Bits: " << colorBits << endl;
-
-    // this should be the highest value that we
-    int totalBits = num_vertices(g) * colorBits;
-    cout << "Total bits for graph: " << totalBits << endl;
-    
-    int numInts = totalBits % UINT_BITS ? totalBits / UINT_BITS + 1 : totalBits / UINT_BITS;
-    cout << "Num ints needed: " << numInts << endl;
-
-    int searchSpaceSize = numInts * sizeof(unsigned int);
-    unsigned int *searchSpace = (unsigned int*)calloc(1, searchSpaceSize);
-    unsigned int *bestSolution = (unsigned int*)calloc(1, searchSpaceSize);
-    if (NULL == searchSpace || NULL == bestSolution) {
-        cout << "Malloc failed!\n";
-        exit(1);
-    }  
-
-    incIntArray(searchSpace, searchSpaceSize);
-    while (!checkIfDone(searchSpace, searchSpaceSize, totalBits)) {
+    int leastConflicts = INT_MAX;
+    int currNumConflicts = INT_MAX;
+    int bestColors[num_vertices(g)] = {0};
+    while (!checkIfDone(g, numColors - 1)) {
         // assign color to each node in graph
         // increment searchSpace
         // track the least number of conflicts
-        cout << "Next coloring" << endl;
-        if (0 == colorGraph(g, searchSpace, searchSpaceSize, colorBits, numColors-1)) {
-            // check if legal color and update best solution
-            cout << "colored" << endl;
-        }
-        incIntArray(searchSpace, searchSpaceSize);
+        incrementColors(g, numColors - 1);
+        printColors(g);
+        /*
+        currNumConflicts = numConflicts(g);
+        if (currNumConflicts < leastConflicts) {
+            leastConflicts = currNumConflicts;
+            int i = 0;
+            pair<Graph::vertex_iterator, Graph::vertex_iterator> vItrRange = vertices(g);
+            for (Graph::vertex_iterator vItr= vItrRange.first; vItr != vItrRange.second; ++vItr) {
+                bestColors[i++] = g[*vItr].color;
+            }
+        } 
+        */
     }
+    return leastConflicts;
 }
 
 int main(int argc, char **argv)
