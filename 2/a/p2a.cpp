5b019ed96c973f0415af0d4d19b943a8e1e5508e
johnvtan <john.v.tan@gmail.com>
2018-05-20 21:10:28 -0400

    updated project 2 files

---
+++ 2/a/p2a.cpp
@@ -0,0 +1,64 @@
+// Project 1a: Solving knapsack using exhaustive search
+//
+
+#include <iostream>
+#include <limits.h>
+#include <list>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+#include "d_except.h"
+#include "d_matrix.h"
+#include "knapsack.h"
+
+void greedyKnapsack(knapsack &k) {
+	return;
+}
+
+int main(int argc, char **argv)
+{
+   ifstream fin;
+   stack <int> moves;
+   string fileName;
+
+   if (2 != argc) 
+   {
+      cout << "Error: need to include path to input as argument.\n"
+           << "\tp1a.out <input-file>\n";
+      exit(1);
+   }
+
+   fin.open(argv[1]);
+   if (!fin)
+   {
+      cerr << "Cannot open " << fileName << endl;
+      exit(1);
+   }
+
+   try
+   {
+      //cout << "Reading knapsack instance" << endl;
+      knapsack k(fin);
+
+      //exhaustiveKnapsack(k, 600);
+
+      //cout << endl << "Best solution" << endl;
+      //k.printSolution();
+      greedyKnapsack(k);
+   }    
+
+   catch (indexRangeError &ex) 
+   { 
+      cout << ex.what() << endl; exit(1);
+   }
+   catch (rangeError &ex)
+   {
+      cout << ex.what() << endl; exit(1);
+   }
+}
