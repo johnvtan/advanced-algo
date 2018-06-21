5e164abb6ff5ad4f883ec4ded322e87529495e04
Emerson Boyd <emerson.boyd@yahoo.com>
2018-06-20 21:55:10 -0400

    Run the python script for knapsack steepest descent and produce a results5.txt file

--- 5/a/steepestDescent/run.py
+++ 5/a/steepestDescent/run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 
-rel_path = '../../knapsack-instances/'
+rel_path = '../../../knapsack-instances/'
 for filename in os.listdir(rel_path):
 	name = filename.split('.')
 	exe_str = './p1a.out ' + rel_path + filename + ' > output/' + name[0] + '.output' 
